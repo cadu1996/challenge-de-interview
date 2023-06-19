23fbf8ec10f0179886155aa1b7ece357db35826d
cadu1996 <cadu1996>
2023-06-19 23:50:40 +0000

    raw to prepared

---
+++ data_engineering/raw_to_prepared/failure_log_table.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+import os
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+# COMMAND ----------
+
+os.path.join("/mnt/", "teste")
+
+# COMMAND ----------
+
+spark = SparkSession.builder.appName("Raw to Prepared").getOrCreate()
+
+# COMMAND ----------
+
+PATH_FROM = "/mnt/lake/raw/"
+PATH_TO = "/mnt/lake/prepared/"
+
+# COMMAND ----------
+
+equipment_failure_sensors_df = spark.read.parquet(os.path.join(PATH_FROM, "equipment_failure_sensors"))
+equipment_sensors_df = spark.read.parquet(os.path.join(PATH_FROM, "equipament_sensors"))
+equipament_df = spark.read.parquet(os.path.join(PATH_FROM, "equipament"))
+
+# COMMAND ----------
+
+fact_table_df = equipment_failure_sensors_df.join(equipment_sensors_df, "sensor_id", "left") \
+    .join(equipament_df, "equipment_id", "left")
+    .select(col("sensor_id"), col("name").alias("equipment_name"), col("group_name").alias("equipment_group_name"), "timestamp", "level")
+
+fact_table_df.write.mode("overwrite").partitionBy("equipment_id", "sensor_id").format("delta").save(PATH_TO)
+
+display(fact_table.limit(10))
