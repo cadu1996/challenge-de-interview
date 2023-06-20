48146e623810be96b12b5e64c22d8b13911217d1
cadu1996 <cadu1996>
2023-06-20 00:01:14 +0000

    fix name partition

--- data_engineering/raw_to_prepared/failure_log_table.py
+++ data_engineering/raw_to_prepared/failure_log_table.py
@@ -30,6 +30,6 @@ fact_table_df = equipment_failure_sensors_df.join(equipment_sensors_df, "sensor_
     .join(equipament_df, "equipment_id", "left") \
     .select(col("sensor_id"), col("name").alias("equipment_name"), col("group_name").alias("equipment_group_name"), "timestamp", "level")
 
-fact_table_df.write.mode("overwrite").partitionBy("equipment_id", "sensor_id").format("delta").save(os.path.join(PATH_TO, "failure_log_table"))
+fact_table_df.write.mode("overwrite").partitionBy("equipment_group_name", "equipment_name", "sensor_id").format("delta").save(os.path.join(PATH_TO, "failure_log_table"))
 
 display(fact_table.limit(10))
